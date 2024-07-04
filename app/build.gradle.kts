c4853e05310c6ebda273ba31da1c2a699d14729f
Tyagi <ayusht262@gmail.com>
2024-07-05 00:03:10 +0530

    FINAL COMMIT

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,49 +16,51 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+    buildFeatures {
+        viewBinding = true
+    }
+        buildTypes {
+            release {
+                isMinifyEnabled = false
+                proguardFiles(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            }
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+        kotlinOptions {
+            jvmTarget = "1.8"
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+
+    dependencies {
+        implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+        implementation("com.squareup.retrofit2:retrofit:2.9.0")
+        implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+        implementation("com.github.bumptech.glide:glide:4.12.0")
+        annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
+        // RecyclerView for displaying lists
+        implementation("androidx.recyclerview:recyclerview:1.2.1")
+
+        // Coroutine support
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
+
+        // Lifecycle components
+        implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+        implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+        implementation("androidx.recyclerview:recyclerview:1.2.1")
+        implementation("androidx.vectordrawable:vectordrawable:1.1.0")
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.appcompat)
+        implementation(libs.material)
+        implementation(libs.androidx.activity)
+        implementation(libs.androidx.constraintlayout)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-dependencies {
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.0")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
-
-    // RecyclerView for displaying lists
-    implementation ("androidx.recyclerview:recyclerview:1.2.1")
-
-    // Coroutine support
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.0")
-
-    // Lifecycle components
-    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
-    implementation ("androidx.recyclerview:recyclerview:1.2.1")
-    implementation ("androidx.vectordrawable:vectordrawable:1.1.0")
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-}
--- app/src/main/java/com/vishu/newflowers/FlowerApi.kt
+++ app/src/main/java/com/vishu/newflowers/FlowerApi.kt
@@ -4,6 +4,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface FlowerApi {
-    @GET("/86dc0d4f-652d-4fc3-80d3-573d02697bb8")
+    @GET("86dc0d4f-652d-4fc3-80d3-573d02697bb8")
     fun getFlowers(): Call<List<Flower>>
 }
